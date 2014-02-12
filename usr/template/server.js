55b0e77183c2ff771fae2dc29fe42738bc01e985
Wesley Hearn <whearn@redhat.com>
2014-02-12 12:32:08 -0500

    Hopefully fix the initial socket.io connection problem

--- usr/template/server.js
+++ usr/template/server.js
@@ -71,7 +71,7 @@ OSBS.server.http.on('listening', serverListening);
 function expressSettings() {
     app.use(function(req, res, next) {
         res.header('Access-Control-Allow-Credentials', true);
-        res.header('Access-Control-Allow-Origin', req.headers.origin);
+        res.header('Access-Control-Allow-Origin', process.env.OPENSHIFT_GEAR_DNS);
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
         res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
         if ('OPTIONS' == req.method) {
